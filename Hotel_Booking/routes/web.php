4cb3c3286f93bbbd3be51a92fa51b2798b42cd64
kevin <kevinkalenga10@gmail.com>
2025-10-26 02:56:03 +0100

    Laravel multi auth with breeze part3

--- Hotel_Booking/routes/web.php
+++ Hotel_Booking/routes/web.php
@@ -20,4 +20,12 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
- Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+// Admin Groupe Middleware
+
+Route::middleware(['auth',  'adminRole:admin'])->group(function() {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
+
+
+
+
