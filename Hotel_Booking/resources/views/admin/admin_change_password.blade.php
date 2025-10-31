0f0a88840a5b9f55e5ade8a6b3d70113df75b936
kevin <kevinkalenga10@gmail.com>
2025-10-31 01:24:42 +0100

    Admin profile change password part1

--- Hotel_Booking/resources/views/admin/admin_change_password.blade.php
+++ Hotel_Booking/resources/views/admin/admin_change_password.blade.php
@@ -6,13 +6,13 @@
 	<div class="page-content">
 				<!--breadcrumb-->
 				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-					<div class="breadcrumb-title pe-3">Admin Profile</div>
+					<div class="breadcrumb-title pe-3">Admin Change Password</div>
 					<div class="ps-3">
 						<nav aria-label="breadcrumb">
 							<ol class="breadcrumb mb-0 p-0">
 								<li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
 								</li>
-								<li class="breadcrumb-item active" aria-current="page">Admin Profile</li>
+								<li class="breadcrumb-item active" aria-current="page">Admin Change Password</li>
 							</ol>
 						</nav>
 					</div>
@@ -66,55 +66,47 @@
 								@csrf
 								<div class="card">
 									<div class="card-body">
+										
 										<div class="row mb-3">
 											<div class="col-sm-3">
-												<h6 class="mb-0">Name</h6>
+												<h6 class="mb-0">Old Password</h6>
 											</div>
 											<div class="col-sm-9 text-secondary">
-												<input type="text" name="name" class="form-control" value="{{$profileData->name}}" />
+												<input type="password" name="old_password" class="form-control
+												 @error('old_password') is-invalid @enderror" id="old_password" />
+												 @error('old_password') 
+                                                   <span class="text-danger">{{$message}}</span>
+												 @enderror
 											</div>
 										</div>
 										<div class="row mb-3">
 											<div class="col-sm-3">
-												<h6 class="mb-0">Email</h6>
+												<h6 class="mb-0">New Password</h6>
 											</div>
 											<div class="col-sm-9 text-secondary">
-												<input type="text" name="email" class="form-control" value="{{$profileData->email}}" />
-											</div>
-										</div>
-										<div class="row mb-3">
-											<div class="col-sm-3">
-												<h6 class="mb-0">Phone</h6>
-											</div>
-											<div class="col-sm-9 text-secondary">
-												<input type="text" name="phone" class="form-control" value="{{$profileData->phone}}" />
+												<input type="password" name="new_password" class="form-control
+												 @error('new_password') is-invalid @enderror" id="new_password" />
+												 @error('new_password') 
+                                                   <span class="text-danger">{{$message}}</span>
+												 @enderror
 											</div>
 										</div>
 										
 										<div class="row mb-3">
 											<div class="col-sm-3">
-												<h6 class="mb-0">Address</h6>
+												<h6 class="mb-0">Confirm New Password</h6>
 											</div>
 											<div class="col-sm-9 text-secondary">
-												<input type="text" name="address" class="form-control" value="{{$profileData->address}}" />
+												<input type="password" name="new_password_confirmation" class="form-control" 
+												id="new_password_confirmation" />
+												
 											</div>
 										</div>
-										<div class="row mb-3">
-											<div class="col-sm-3">
-												<h6 class="mb-0">Photo</h6>
-											</div>
-											<div class="col-sm-9 text-secondary">
-												<input type="file" name="photo" class="form-control" id="image" value="{{$profileData->photo}}" />
-											</div>
-										</div>
-										<div class="row mb-3">
-											<div class="col-sm-3">
-												<h6 class="mb-0"></h6>
-											</div>
-											<div class="col-sm-9 text-secondary">
-												<img id="showImage" src="{{(!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/default_avatar.jpg')}}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
-											</div>
-										</div>
+										
+										
+										
+										
+										
 										<div class="row">
 											<div class="col-sm-3"></div>
 											<div class="col-sm-9 text-secondary">
@@ -130,16 +122,6 @@
 				</div>
 			</div>
 
-			<script type="text/javascript">
-               $(document).ready(function(){
-				$('#image').change(function(e){
-					var reader = new FileReader();
-					reader.onload = function(e){
-						$("#showImage").attr('src', e.target.result)
-					}
-					reader.readAsDataURL(e.target.files['0'])
-				})
-			   })
-			</script>
+			
 
 @endsection
