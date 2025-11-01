001f539c5fe522992c1b6dc5d19cb7de70ee011d
kevin <kevinkalenga10@gmail.com>
2025-11-01 16:44:41 +0100

    User profile design part1

--- Hotel_Booking/app/Http/Controllers/UserController.php
+++ Hotel_Booking/app/Http/Controllers/UserController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,5 +11,13 @@ class UserController extends Controller
     public function index()
     {
         return view('frontend.index');
+    }
+
+    public function userProfile()
+    {
+        //  get the user who is authenticated by id 
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('frontend.dashboard.edit_profile', compact('profileData'));
     }
 }
