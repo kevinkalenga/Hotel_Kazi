9afa9468f7e73448ec2007fb87fbd4d72fc7e158
kevin <kevinkalenga10@gmail.com>
2025-11-03 23:46:44 +0100

    Install image intervention package

--- Hotel_Booking/resources/views/auth/login.blade.php
+++ Hotel_Booking/resources/views/auth/login.blade.php
@@ -32,6 +32,29 @@
                                 
                                 <form method="POST" action="{{ route('login') }}">
                                     @csrf
+                                    
+                                    
+                                      @if (session('status'))
+                                           <div class="alert alert-success">
+                                               {{ session('status') }}
+                                           </div>
+                                       @endif
+
+                                       @if ($errors->any())
+                                           <div class="alert alert-danger">
+                                               <ul class="mb-0">
+                                                   @foreach ($errors->all() as $error)
+                                                       <li>{{ $error }}</li>
+                                                   @endforeach
+                                               </ul>
+                                           </div>
+                                       @endif
+
+                                    
+                                    
+                                    
+                                    
+                                    
                                     <div class="row">
                                         <div class="col-lg-12 ">
                                             <div class="form-group">
