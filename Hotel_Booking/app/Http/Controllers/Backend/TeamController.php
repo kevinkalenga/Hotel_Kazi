539b441e6ab781104c9d5275c237e756335438a0
kevin <kevinkalenga10@gmail.com>
2025-11-05 01:34:25 +0100

    Book area setup part2

--- Hotel_Booking/app/Http/Controllers/Backend/TeamController.php
+++ Hotel_Booking/app/Http/Controllers/Backend/TeamController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Team;
+use App\Models\BookArea;
 use Carbon\Carbon;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
@@ -180,7 +181,8 @@ class TeamController extends Controller
 
     public function BookArea()
     {
-        
+        $book = BookArea::find(1);
+        return view('backend.bookarea.book_area', compact('book'));
     }
 
 
