742c04039eb15a1834908e398d6cf6a275c71cd9
kevin <kevinkalenga10@gmail.com>
2025-11-07 00:46:08 +0100

    RoomType Area Setup Part2

--- Hotel_Booking/app/Http/Controllers/Backend/RoomTypeController.php
+++ Hotel_Booking/app/Http/Controllers/Backend/RoomTypeController.php
@@ -16,4 +16,23 @@ class RoomTypeController extends Controller
       $allData = RoomType::orderBy('id', 'desc')->get();
       return view('backend.allroom.roomtype.view_roomtype', compact('allData'));
     }
+    public function AddRoomType()
+    {
+      $allData = RoomType::orderBy('id', 'desc')->get();
+      return view('backend.allroom.roomtype.add_roomtype');
+    }
+    public function RoomTypeStore(Request $request)
+    {
+      RoomType::insert([
+        'name' => $request->name,
+        'created_at' => Carbon::now()
+      ]);
+
+      $notification = array(
+           'message' => 'RoomType Inserted Successfully',
+           'alert-type' => 'success'
+        );
+
+       return redirect()->route('room.type.list')->with($notification);
+    }
 }
--- Hotel_Booking/routes/web.php
+++ Hotel_Booking/routes/web.php
@@ -67,6 +67,8 @@ Route::middleware(['auth',  'adminRole:admin'])->group(function() {
     Route::controller(RoomTypeController::class)->group(function() {
        
         Route::get('/room/type/list', 'RoomTypeList')->name('room.type.list');
+        Route::get('/add/room/type', 'AddRoomType')->name('add.room.type');
+        Route::post('/room/type/store', 'RoomTypeStore')->name('room.type.store');
       
     });
 
