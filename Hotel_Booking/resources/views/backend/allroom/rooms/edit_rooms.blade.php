bd092159e35bdc9ceacd0ac896922cad710aff51
kevin <kevinkalenga10@gmail.com>
2025-11-10 13:49:44 +0100

    Edit Room Setup Part6

--- Hotel_Booking/resources/views/backend/allroom/rooms/edit_rooms.blade.php
+++ Hotel_Booking/resources/views/backend/allroom/rooms/edit_rooms.blade.php
@@ -57,11 +57,14 @@
 									</div>
 									<div class="col-md-6">
 										<label for="input3" class="form-label">Main Image</label>
-										<input type="file" name="image" class="form-control" id="input3" placeholder="Phone">
+										<input type="file" name="image" class="form-control" id="image" placeholder="Phone">
+										<img id="showImage" src="{{(!empty($editData->image)) ? url('upload/rooming/'.$editData->image) : url('upload/default_avatar.jpg')}}" alt="Admin" class="p-1 my-2 bg-primary" width="60">
 									</div>
 									<div class="col-md-6">
 										<label for="input4" class="form-label">Gallery Image</label>
-										<input type="file" name="multi_img[]" class="form-control" id="multiImg" accept="image/jpeg, image/jpg, image/gif, image/png">
+										<input multiple type="file" name="multi_img[]" class="form-control" id="multiImg" accept="image/jpeg, image/jpg, image/gif, image/png">
+
+										<div class="row" id="preview_img"></div>
 									</div>
 									
 									<div class="col-md-4">
@@ -112,8 +115,8 @@
 									
 									<div class="col-md-12">
 										<div class="d-md-flex d-grid align-items-center gap-3">
-											<button type="button" class="btn btn-primary px-4">Submit</button>
-											<button type="button" class="btn btn-light px-4">Reset</button>
+											<button type="submit" class="btn btn-primary px-4">Change Save</button>
+											
 										</div>
 									</div>
 								</form>
@@ -158,6 +161,46 @@
 	</div>
 
 
+    	<script type="text/javascript">
+               $(document).ready(function(){
+				$('#image').change(function(e){
+					var reader = new FileReader();
+					reader.onload = function(e){
+						$("#showImage").attr('src', e.target.result)
+					}
+					reader.readAsDataURL(e.target.files['0'])
+				})
+			   })
+		</script>
 
+
+        <!--------===Show MultiImage ========------->
+<script>
+   $(document).ready(function(){
+    $('#multiImg').on('change', function(){ //on file input change
+       if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+       {
+           var data = $(this)[0].files; //this file data
+            
+           $.each(data, function(index, file){ //loop though each file
+               if(/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)){ //check supported file type
+                   var fRead = new FileReader(); //new filereader
+                   fRead.onload = (function(file){ //trigger function on successful read
+                   return function(e) {
+                       var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(100)
+                   .height(80); //create image element 
+                       $('#preview_img').append(img); //append image to output element
+                   };
+                   })(file);
+                   fRead.readAsDataURL(file); //URL representing the file's data.
+               }
+           });
+            
+       }else{
+           alert("Your browser doesn't support File API!"); //if File API is absent
+       }
+    });
+   });
+</script>
 
 @endsection
