e1b22f28a93a7ba284ff8593b04db8d76ffb9225
kevin <kevinkalenga10@gmail.com>
2026-02-13 12:15:26 +0100

    Check Room Availability Part6

--- Hotel_Booking/app/Models/Booking.php
+++ Hotel_Booking/app/Models/Booking.php
@@ -8,4 +8,9 @@ class Booking extends Model
 {
      //All our field will be fillable
      protected $guarded = [];
+
+     public function assign_rooms()
+     {
+          return $this->hasMany(BookingRoomList::class, 'booking_id');
+     }
 }
