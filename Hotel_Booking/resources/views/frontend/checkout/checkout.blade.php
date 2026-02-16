bdc5be53573612fe90afc56bed823d319c054308
kevin <kevinkalenga10@gmail.com>
2026-02-16 15:52:46 +0100

    Complete Room Checkout Part3

--- Hotel_Booking/resources/views/frontend/checkout/checkout.blade.php
+++ Hotel_Booking/resources/views/frontend/checkout/checkout.blade.php
@@ -130,11 +130,13 @@
                                             <hr>
               
                                             <div style="display: flex">
-                                                  <img style="height:100px; width:120px;object-fit: cover" src=" " alt="Images" alt="Images">
+												  <img style="height:100px; width:120px; object-fit: cover;"
+                                                      src="{{ (!empty($room->image)) ? asset($room->image) : asset('upload/default_avatar.jpg') }}" 
+                                                      alt="Room Image"/>
                                                   <div style="padding-left: 10px;">
-                                                        <a href=" " style="font-size: 20px; color: #595959;font-weight: bold">Room Name</a>
-                                                        <p><b>120 / Night</b></p>
-                                                  </div>
+                                                        <a href=" " style="font-size: 20px; color: #595959;font-weight: bold">{{ @$room->type->name }}</a>
+                                                        <p><b>{{ $room->price }} / Night</b></p>
+                                                  </div> 
               
                                             </div>
               
