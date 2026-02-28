f334544db84383b7bf9c4f61692863c48448c89a
kevin <kevinkalenga10@gmail.com>
2026-02-28 23:42:04 +0100

    Backend Booking Request Part9

--- Hotel_Booking/resources/views/backend/booking/edit_booking.blade.php
+++ Hotel_Booking/resources/views/backend/booking/edit_booking.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_dashboard')
 @section('admin') 
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <div class="page-content">
     <div class="row row-cols-1 row-cols-md-2 row-cols-xl-5">
        
@@ -208,16 +208,17 @@
                       <div class="row ">
                          <div class="col-md-12 mb-2">
                             <label for="">CheckIn</label>
-                            <input type="date" name="check_in" class="form-control" required value="{{$editData->check_in}}">
+                            <input type="date" name="check_in" class="form-control" required value="{{$editData->check_in}}" id="check_in">
                          </div>
                          <div class="col-md-12 mb-2">
                             <label for="">CheckOut</label>
-                            <input type="date" name="check_out" class="form-control" required value="{{$editData->check_out}}">
+                            <input type="date" name="check_out" class="form-control" required value="{{$editData->check_out}}" id="check_out">
                          </div>
                          <div class="col-md-12 mb-2">
                             <label for="">Room</label>
                             <input type="number" name="number_of_rooms" class="form-control" required value="{{$editData->number_of_rooms}}">
                          </div>
+                         <input type="hidden" id="available_room" name="available_room" class="form-control" vale="{{$editData->numer_of_rooms}}">
                          <div class="col-md-12 mb-2">
                             <label for="">Availability: <span class="text-success availability"></span></label>
                             
@@ -271,10 +272,33 @@
     </div><!--end row-->
 
      
-
-       
-
-         
+  
 </div>
+
+<script>
+  $(document).ready(function () {
+      getAvaility()
+   });
+  
+    function getAvaility() {
+       var check_in = $('#check_in').val();
+       var check_out = $('#check_out').val();
+       var room_id = {{$editData->rooms_id}};
+
+        $.ajax({
+         url: "{{ route('check_room_availability') }}",
+         data: {room_id:room_id, check_in:check_in, check_out:check_out},
+         success: function(data){
+             $(".availability").text(data['available_room']);
+             $("#available_room").val(data['available_room']);
+            
+          }
+        })
+    }
+
+
+  
+ 
+</script>
 
 @endsection
