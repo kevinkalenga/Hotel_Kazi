905ba2eb7addd9aa28b98f41c99aa1beaa6b5782
kevin <kevinkalenga10@gmail.com>
2026-03-31 10:15:19 +0200

    Show Blog Comment in frontend Page Part3

--- Hotel_Booking/app/Models/Comment.php
+++ Hotel_Booking/app/Models/Comment.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
      protected $guarded = [];
+
+     /**
+      * Récupère l'utilisateur associé à ce commentaire.
+      *
+      * Cette méthode définit une relation inverse "belongsTo" :
+      * chaque commentaire appartient à un seul utilisateur.
+      * La clé étrangère est 'user_id' dans la table comments,
+      * qui fait référence à la colonne 'id' de la table users.
+      */
+
+     public function user()
+     {
+          return $this->belongsTo(User::class, 'user_id', 'id');
+     }
 }
