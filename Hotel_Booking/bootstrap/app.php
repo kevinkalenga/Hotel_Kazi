011cec63f048b49ff13c90b037d5be94a82d579e
kevin <kevinkalenga10@gmail.com>
2026-06-09 11:46:43 +0200

    Add Role And Permission for Admin User Part2

--- Hotel_Booking/bootstrap/app.php
+++ Hotel_Booking/bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
             $middleware->alias([
           'adminRole' => \App\Http\Middleware\AdminRole::class,
+          'role' => RoleMiddleware::class,
+          'permission' => PermissionMiddleware::class,
+          'role_or_permission' => RoleOrPermissionMiddleware::class,
          
         ]);
     })
