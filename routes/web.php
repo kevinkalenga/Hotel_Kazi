7c0cb7cd8f2084dc21c6e343cf7bcf92a8b89fb2
kevin <kevinkalenga10@gmail.com>
2026-06-10 01:41:09 +0200

    fix railway db crash

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,11 @@ use App\Http\Controllers\Frontend\BookingController;
 //     return view('welcome');
 // });
 
+Route::get('/', function () {
+    return "HOME OK";
+});
 
-
-Route::get('/', [UserController::class, 'index'])->name('index');
+// Route::get('/', [UserController::class, 'index'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('frontend.dashboard.user_dashboard');
